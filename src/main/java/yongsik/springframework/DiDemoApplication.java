eda6dc52a9490afbac61dd50fc2e1f75fed77c3e
admin <admin@HM5F570521>
2018-06-26 19:55:46 +0900

    ＃DI-DEMO External Properties

--- src/main/java/yongsik/springframework/DiDemoApplication.java
+++ src/main/java/yongsik/springframework/DiDemoApplication.java
@@ -3,24 +3,24 @@ package yongsik.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-
-import yongsik.springframework.controllers.ConstructorInjectedController;
-import yongsik.springframework.controllers.GetterInjectedController;
 import yongsik.springframework.controllers.MyController;
-import yongsik.springframework.controllers.PropertyInjectedController;
+import yongsik.springframework.examplebeans.FakeDataSource;
+import yongsik.springframework.examplebeans.FakeJmsBroker;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"yongsik.services", "yongsik.springframework"})
 public class DiDemoApplication {
 	
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		
 		MyController controller = (MyController) ctx.getBean("myController");
-		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
+		
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+		System.out.println(fakeJmsBroker.getUsername());
+		
 	}
 	
 }
